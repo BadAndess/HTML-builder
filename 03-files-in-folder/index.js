af5cb71006df4ba3fcd852beccda85e11bc89d4b
Andess <Andess@tut.by>
2023-05-07 14:54:51 +0300

    feat: add some code

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+const path = require('path');
+const { stdout, stdin} = process;
+const secretFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(secretFolder,  (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+    });
+});
+    
