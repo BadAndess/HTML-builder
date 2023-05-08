c2c470071bade5c2d283bf7469d8af19be14302a
Andess <Andess@tut.by>
2023-05-08 09:58:31 +0300

    feat: edit task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,8 +4,19 @@ const { stdout, stdin} = process;
 const secretFolder = path.join(__dirname, 'secret-folder');
 
 fs.readdir(secretFolder,  (err, files) => {
+    if(err) throw err;
+    
+    console.log(`Info about objects in folder ${secretFolder}:`);
+
     files.forEach(file => {
-        console.log(file);
-    });
+        fs.stat(secretFolder, {withFileTypes: true}, (err, stats) => {
+            if(err) {
+                console.log('File error');
+                return;
+            } else 
+    console.log(`Name ${file} ${stats.size} byte`);
+        })
+    })
 });
+
     
